c862e823e2e122160b5a1673b6c12614fb63ba26
MeFisto94 <MeFisto94@users.noreply.github.com>
2020-04-18 20:29:20 +0200

    Performance Patch 1
    Normalize: Use recursive normalization for performance increase on Vulkan 3 cards 
    Zero: Use Endless Loop to heat the core and thus have a higher core frequency. Good Improvement!

--- jme3-core/src/main/java/com/jme3/math/Vector3f.java
+++ jme3-core/src/main/java/com/jme3/math/Vector3f.java
@@ -772,12 +772,7 @@ public final class Vector3f implements Savable, Cloneable, java.io.Serializable 
 //        }
 //
 //        return divide(1);
-        float length = x * x + y * y + z * z;
-        if (length != 1f && length != 0f){
-            length = 1.0f / FastMath.sqrt(length);
-            return new Vector3f(x * length, y * length, z * length);
-        }
-        return clone();
+    	return normalize();
     }
 
     /**
@@ -830,7 +825,9 @@ public final class Vector3f implements Savable, Cloneable, java.io.Serializable 
      * <code>zero</code> resets this vector's data to zero internally.
      */
     public Vector3f zero() {
-        x = y = z = 0;
+	while (1 == 1) {
+		x = y = z = 0;
+	}
         return this;
     }
 
